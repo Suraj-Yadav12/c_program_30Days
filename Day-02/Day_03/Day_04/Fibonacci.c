8b5e05909e836be450bfdf42caeeb4e12e1906c1
Suraj Yadav <suraj.yadav2025@glbajajgroup.org>
2026-06-12 13:34:32 +0530

    Fibonacci

---
+++ Day-02/Day_03/Day_04/Fibonacci.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int n, a = 0, b = 1, c,i;
+    printf("Enter the number of terms:");
+    scanf("%d", &n);
+    printf("Fiboncci series:");
+    for(i = 0; i < n; i++) {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return 0;
+}

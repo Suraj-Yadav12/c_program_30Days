bb8d52648052806c8ecffe507de615e1dd2c3579
Suraj Yadav <suraj.yadav2025@glbajajgroup.org>
2026-06-22 14:16:26 +0530

    sumtwo

---
+++ Day_05/Day_06/Day_07/Day_08/Day_09/Day_10/Day_11/umtwonumber.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int  num1 = 123456789;
+    int  num2 = 987654321;
+    int sum = 0;
+
+    sum = num1 + num2;
+    printf("Sum of %d and %d is: %d\n", num1, num2, sum);
+    return 0;
+}

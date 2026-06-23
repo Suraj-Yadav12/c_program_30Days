b16fae5100e90cb376664a44121a0277fd6a992b
Suraj Yadav <suraj.yadav2025@glbajajgroup.org>
2026-06-23 11:14:13 +0530

    sumtonumber

--- Day_05/Day_06/Day_07/Day_08/Day_09/Day_10/Day_11/Maximum.c
+++ Day_05/Day_06/Day_07/Day_08/Day_09/Day_10/Day_11/Maximum.c
@@ -4,11 +4,8 @@ int main()
 {
     int num1, num2;
 
-    printf("Enter first number: ");
-    scanf("%d", &num1);
-
-    printf("Enter second number: ");
-    scanf("%d", &num2);
+   
+    
 
     if(num1 > num2)
     {

8a046d0b76563519e1ebe91c89764172fa81d805
Suraj Yadav <suraj.yadav2025@glbajajgroup.org>
2026-06-24 15:16:29 +0530

    Armstron

--- Day_05/Day_06/Day_07/Day_08/Day_09/Day_10/Day_11/day_12/tempCodeRunnerFile.c
+++ Day_05/Day_06/Day_07/Day_08/Day_09/Day_10/Day_11/day_12/tempCodeRunnerFile.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int num, sum = 0;
+    int num, originalNum, remainder;
+    int result = 0;
 
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    for (int i = 1; i < num; i++) {
-        if (num % i == 0) {
-            sum += i;
-        }
+    originalNum = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        result = result + (remainder * remainder * remainder);
+        num = num / 10;
     }
 
-    if (sum == num)
-        printf("%d is a Perfect Number", num);
+    if (result == originalNum)
+        printf("%d is an Armstrong number.", originalNum);
     else
-        printf("%d is not a Perfect Number", num);
+        printf("%d is not an Armstrong number.", originalNum);
 
     return 0;
 }

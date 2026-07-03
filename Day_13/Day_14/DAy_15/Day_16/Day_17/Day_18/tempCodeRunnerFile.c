e19ccde32e4bce42154bc2a1a3069b88db52ffa3
Suraj Yadav <suraj.yadav2025@glbajajgroup.org>
2026-07-03 14:20:38 +0530

    Bubble short

---
+++ Day_13/Day_14/DAy_15/Day_16/Day_17/Day_18/tempCodeRunnerFile.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main() {
+    int arr , i ,j ,n ,temp;
+    printf("Enter the size of array : ");
+    scanf("%d", &n);
+    printf("Enter the elements of array : ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    for(i = 0; i < n-1; i++)
+    {
+        for(j = 0; j < n-1-i; j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    printf("Array after bubble sort : ");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
